8ece39dbd7f31307e3ff5a820f1f695cd29e890f
seokgyo <seokgyo@gmail.com>
2016-03-09 22:51:15 +0900

    sidebar cleanup.

--- react/components/AppSidebar.js
+++ react/components/AppSidebar.js
@@ -1,34 +1,22 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router'
 
 export default React.createClass({
   propTypes: {
     children: PropTypes.node,
   },
-  contextTypes: {
-    router: PropTypes.object.isRequired,
-  },
-  componentDidMount() {
-  },
   render() {
     return (
       <div className="col-sm-3 col-md-2 sidebar">
         <ul className="nav nav-sidebar">
-          <li className="active"><a href="#">Overview <span className="sr-only">(current)</span></a></li>
-          <li><a href="#">Reports</a></li>
-          <li><a href="#">Analytics</a></li>
-          <li><a href="#">Export</a></li>
+          <li className="active">
+            <Link to="/">
+              Order List
+            </Link>
+          </li>
         </ul>
         <ul className="nav nav-sidebar">
           <li><a href="">Nav item</a></li>
-          <li><a href="">Nav item again</a></li>
-          <li><a href="">One more nav</a></li>
-          <li><a href="">Another nav item</a></li>
-          <li><a href="">More navigation</a></li>
-        </ul>
-        <ul className="nav nav-sidebar">
-          <li><a href="">Nav item again</a></li>
-          <li><a href="">One more nav</a></li>
-          <li><a href="">Another nav item</a></li>
         </ul>
       </div>
     );
