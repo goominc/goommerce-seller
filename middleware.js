3858684121a9077fdf3db5f0bd7bef0f5ae06d7f
seokgyo <seokgyo@gmail.com>
2016-03-10 10:38:18 +0900

    ie8 handling and default index redirect.

--- middleware.js
+++ middleware.js
@@ -45,6 +45,12 @@ module.exports = (opts) => {
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>${config.title}</title>
             <link rel="stylesheet" href="${cdn}/vendor/bootstrap-3.3.6/css/bootstrap.min.css"/>
+
+            <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+            <!--[if lt IE 9]>
+              <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+            <![endif]-->
           </head>
           <body>
             <div id='root'></div>
--- react/routes.js
+++ react/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { IndexRedirect, Route } from 'react-router';
 import App from './containers/App';
 import OrderList from './containers/OrderList';
 import SelectBrand from './containers/SelectBrand';
@@ -18,7 +18,7 @@ export default function configure({ getAuth }) {
     const brands = _.filter(getAuth().roles,
       (r) => r.type === 'owner' || r.type === 'staff').map((r) => r.brand);
     if (brands.length === 1) {
-      replace(`/brands/${brands[0].id}`);
+      replace(`/brands/${brands[0].id}/orders`);
     }
   }
 
@@ -26,7 +26,8 @@ export default function configure({ getAuth }) {
     <Route>
       <Route onEnter={redirectToLogin}>
         <Route component={App} path="/brands/:brandId">
-          <Route component={OrderList} path="" />
+          <IndexRedirect to="orders" />
+          <Route component={OrderList} path="orders" />
         </Route>
         <Route component={SelectBrand} path="/" onEnter={redirectToDefault} />
       </Route>
